3b576da10b99b5b877dc70c83458991b6003cded
Gustavo Pombal <pombalgustavo@gmail.com>
2024-09-11 15:45:30 -0300

    Handling Exceptions and clearing the screen

--- src/boardGame/BoardException.java
+++ src/boardGame/BoardException.java
@@ -2,7 +2,7 @@ package boardGame;
 
 public class BoardException extends RuntimeException{
 
-
+    private static final long serialVersionUID = 1L;
     public BoardException(String msg){
         super(msg);
     }
